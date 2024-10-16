8506fa2fe134658e36a954f12e79f57c8f4cc624
teradakyouhei <fbosaitera@gmail.com>
2024-10-17 00:15:19 +0900

    PHP課題7提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP課題7</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+
+        //  連想配列に値を代入する
+         $Array = ['name' => 'onion', ' price' => 200, 'weight' => 160]; 
+
+         print_r($Array)
+
+         ?>
+     </p>
+ </body>
+ 
+ </html>
